be63633e8451c1477f7c3e73a52acfac8efe492b
maxmud06 <mahmud9i2006@gmail.com>
2025-04-11 12:09:50 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import telebot
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Добро пожаловать в PIXEL CS2 MARKET!")
+
+bot.infinity_polling()
