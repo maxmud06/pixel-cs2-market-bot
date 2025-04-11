5df4829331219baef72a8bb395432860fdc39e22
maxmud06 <mahmud9i2006@gmail.com>
2025-04-11 15:53:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import telebot
 
+# Telegram bot token
 TOKEN = "8002268236:AAFDlQpt_GS85hyOG9B_5_67tnGYEMLZF80"
 bot = telebot.TeleBot(TOKEN)
 
+# Start komandasi
 @bot.message_handler(commands=['start'])
-def welcome(message):
-    bot.send_message(message.chat.id, "Assalomu alaykum! PIXEL CS2 MARKET botiga xush kelibsiz!")
+def start_handler(message):
+    text = "Assalomu alaykum, bu PIXEL CS2 Market bot!\nSizga qanday yordam bera olaman?"
+    bot.send_message(message.chat.id, text)
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
+# Har qanday matnga javob
+@bot.message_handler(func=lambda m: True)
+def echo_handler(message):
     bot.send_message(message.chat.id, "Siz yozdingiz: " + message.text)
 
+# Botni ishga tushirish
 bot.infinity_polling()
