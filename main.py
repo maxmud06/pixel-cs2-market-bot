48cc4761d9d70e79d100494994a51ae924f3a407
maxmud06 <mahmud9i2006@gmail.com>
2025-04-11 15:46:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import telebot
-import os
 
-8002268236:AAFDlQpt_GS85hyOG9B_5_67tnGYEMLZF80 = os.getenv("8002268236:AAFDlQpt_GS85hyOG9B_5_67tnGYEMLZF80")
-bot = telebot.TeleBot(8002268236:AAFDlQpt_GS85hyOG9B_5_67tnGYEMLZF80)
+TOKEN = "8002268236:AAFDlQpt_GS85hyOG9B_5_67tnGYEMLZF80"
+bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, "Добро пожаловать в PIXEL CS2 MARKET!")
+def welcome(message):
+    bot.send_message(message.chat.id, "Assalomu alaykum! PIXEL CS2 MARKET botiga xush kelibsiz!")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.send_message(message.chat.id, "Siz yozdingiz: " + message.text)
 
 bot.infinity_polling()
